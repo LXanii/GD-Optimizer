cc1e24eff0b4739b77bd7be8c66aa12df533bcf0
Xanii <73562093+LXanii@users.noreply.github.com>
2022-11-29 14:16:05 -0500

    wip 2.0.0

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import os
 import time
+import wmi
 
-big_ver = "1"
-mini_ver = ".1.0"
-version = "(" + big_ver + mini_ver + ")"
+# For 2.0.0, make into a dll with more optimizations
 
+pcount = 0
+
+processes = wmi.WMI ()
 """
-Priority Level Value	Priority Level Name
+for process in processes.Win32_Process(name="discord.exe"):
+    print(process.ProcessId, process.Name)
+    pcount += 1
 
-256	                    Realtime
-128	                    High
-32768	                Above normal
-32	                    Normal
-16384	                Below normal
-64	                    Low
+if pcount == 0:
+    print("No optimizable process found. [discord.exe]")
+else:
+    print("Processes found. [discord.exe]", "(" + str(pcount) + ")")
+
+for process in processes.Win32_Process(name="firefox.exe"):
+    print(process.ProcessId, process.Name)
+    pcount += 1
+
+if pcount == 0:
+    print("No optimizable process found. [firefox.exe]")
+else:
+    print(str(pcount) + " Processes found. [firefox.exe]")
 """
+# finds processes ^^^
+
+red = "\033[1;31;40m"
+white = "\033[1;37;40m"
+first = True
+big_ver = "2"
+mini_ver = ".0.0"
+version = white + " (" + big_ver + mini_ver + ")"
+title = white + "GD Optimizer Version"
 
 # sets these processes to their respective priorities
 low = ["Discord.exe", "steamwebhelper.exe", "firefox.exe", "chrome.exe"]
@@ -51,13 +71,25 @@ def realpriority(): # sets to high for some reason lmfao
         os.system('cls')
 
 def process():
+    time.sleep(1)
     realpriority()
-    #highpriority()
+    #highpriority() these 2 do nothing as of now
     #normalpriority()
     lowpriority()
-    return("All processes set correctly.")
+    return(white + "\nAll processes set correctly.")
 
-print("\033[1;37;40mGD Optimizer Version", version , "\n\033[1;31;40mWarning: You will need to relaunch \nthis everytime you open GD, or join a new discord call.", "\033[1;37;40m\n" + process())
-time.sleep(1)
-print("Closing in 5 seconds...")
-time.sleep(5)
+# work here on finding processes vvvv
+
+while True:
+    if first == True:
+        print(title + version, red + "\n\nWarning: You will need to relaunch \nthis everytime you open GD.\n", process() + "\n")
+        first = False
+        time.sleep(1)
+    else:
+        print(title + version, red + "\n\nWarning: You will need to relaunch \nthis everytime you open GD.\n")
+    print(white + "Looping Discord Priority... \n\n" + red + "Do not exit the program.")
+    time.sleep(5)
+    i = "Discord.exe"
+    process_change = 'wmic process where name="' + i + '" CALL setpriority "64"'
+    os.system(process_change)
+    os.system('cls')
