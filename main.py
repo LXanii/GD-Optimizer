0aa0ef6815cb4d69e05a439f618e8e25c820c08a
Xanii <73562093+LXanii@users.noreply.github.com>
2022-11-30 16:13:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+from colorama import Fore, init
 import os
 import time
 import wmi
 
 # For 2.0.0, make into a dll with more optimizations
 
-red = "\033[1;31;40m"
-white = "\033[1;37;40m"
+init() # used cuz colors just didnt work before and google said this fixed it
+
 first = True
 close = False
 pcount = 0
@@ -16,12 +17,11 @@ title = "GD Optimizer Version"
 processes = wmi.WMI()
 
 # sets these processes to their respective priorities
-low = ["Discord.exe", "steamwebhelper.exe", "firefox.exe", "chrome.exe"]
+low = ["Discord.exe", "steamwebhelper.exe", "firefox.exe", "chrome.exe", "Spotify.exe", "MsMpEng.exe"]
 normal = []
-high = []
-real = ["GeometryDash.exe"]
+high = ["GeometryDash.exe"]
 
-#####################################
+# MsMpEng.exe is windows defender service, which in cases can lag.
 
 def lowpriority():
     for i in low:
@@ -44,20 +44,12 @@ def highpriority():
         os.system(process_change)
         os.system('cls')
 
-def realpriority(): # sets to high for some reason lmfao
-    for i in real:
-        process_change = 'wmic process where name="' + i + '" CALL setpriority "256"'
-        print(process_change)
-        os.system(process_change)
-        os.system('cls')
-
 def priority_change():
     time.sleep(1)
-    realpriority()
-    #highpriority() these 2 do nothing as of now
-    #normalpriority()
+    highpriority()
+    #normalpriority() this is used for nothing rn
     lowpriority()
-    return(white + "\nAll processes set correctly.")
+    return(Fore.WHITE + "\nAll processes set correctly.")
 
 # functions above ^^^^^^^
 
@@ -67,44 +59,45 @@ for i in low:
     for process in processes.Win32_Process(name=i):
         pcount += 1
     if pcount == 0:
-        print("No optimizable process found. " + "[" + i + "]")
+        print(Fore.RED + "No optimizable process found. " + "[" + i + "]")
         low.remove(i)
     else:
-        print(str(pcount) + " Processes found. " + "[" + i + "]")
+        print(Fore.GREEN + str(pcount) + " Processes found. " + "[" + i + "]")
     pcount = 0
 
-for i in real:
+for i in high:
     for process in processes.Win32_Process(name=i):
         pcount += 1
     if pcount == 0:
-        print("No optimizable processes found. " + "[" + i + "]")
+        print(Fore.RED + "No optimizable process found. " + "[" + i + "]")
         if i == "GeometryDash.exe":
             close = True
-            real.remove(i)
+            high.remove(i)
         else:
-            real.remove(i)
+            high.remove(i)
     else:
-        print(str(pcount) + " Processes found. " + "[" + i + "]")
+        print(Fore.GREEN + str(pcount) + " Processes found. " + "[" + i + "]")
     pcount = 0
 
-print("\nOptimizing...")
+print("\n" + Fore.WHITE + "Optimizing...\n")
 time.sleep(2)
 
 while True:
     if close == False:
         if first == True:
-            print(white + title + white + version, red + "\n\nWarning: You will need to relaunch \nthis everytime you open GD.\n", priority_change() + "\n")
+            print(Fore.WHITE + title + Fore.WHITE + version, Fore.RED + "\n\nWarning: You will need to relaunch \nthis everytime you open GD.\n", priority_change() + "\n")
             first = False
             time.sleep(1)
         else:
-            print(white + title + white + version, red + "\n\nWarning: You will need to relaunch \nthis everytime you open GD.\n")
-        print(white + "Looping Discord Priority... \n\n" + red + "Do not exit the program.")
+            print(Fore.WHITE + title + Fore.WHITE + version, Fore.RED + "\n\nWarning: You will need to relaunch \nthis everytime you open GD.\n")
+        print(Fore.WHITE + "Looping Discord Priority... \n\n" + Fore.RED + "Do not close this window.")
         time.sleep(5)
         i = "Discord.exe"
         process_change = 'wmic process where name="' + i + '" CALL setpriority "64"'
         os.system(process_change)
         os.system('cls')
     else:
-        print(red + "\nGeometryDash.exe was not found. \n\nClosing Program...")
+        error = "GeometryDash.exe was not found." + "\n\n" + Fore.RED + "Closing Program..."
+        print(Fore.YELLOW + str(error))
         time.sleep(3)
         break
