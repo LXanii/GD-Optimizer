ab5c9ea6a6f0679cc3ef4e2ae73363a453bffa7b
Hong <107442044+HongGong232@users.noreply.github.com>
2022-12-17 23:22:55 +0000

    Updated Version of GD Optimizer
    the exe file is not updated

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
 import time
+import shutil
+
+folder = 'C:/Users/'+os.getlogin()+'/AppData/Local/Temp'
+
+deleteFileCount = 0
+deleteFolderCount = 0
 
 big_ver = "1"
 mini_ver = ".1.0"
@@ -7,7 +13,6 @@ version = "(" + big_ver + mini_ver + ")"
 
 """
 Priority Level Value	Priority Level Name
-
 256	                    Realtime
 128	                    High
 32768	                Above normal
@@ -58,6 +63,29 @@ def process():
     return("All processes set correctly.")
 
 print("\033[1;37;40mGD Optimizer Version", version , "\n\033[1;31;40mWarning: You will need to relaunch \nthis everytime you open GD, or join a new discord call.", "\033[1;37;40m\n" + process())
+print("Running Temp Cleaner...")
+time.sleep(3)
+
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    indexNo = file_path.find('\\')
+    itemName = file_path[indexNo+1:]
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+            print( '%s file deleted' % itemName )
+            deleteFileCount = deleteFileCount + 1
+
+        elif os.path.isdir(file_path):
+            if file_path.__contains__('chocolatey'):  continue
+            shutil.rmtree(file_path)
+            print( '%s folder deleted' % itemName )
+            deleteFolderCount = deleteFolderCount + 1
+
+    except Exception as e:
+        print('Access Denied: %s' % itemName )
+
+print(str(deleteFileCount)+' files and '+ str(deleteFolderCount)+' folders deleted.')
 time.sleep(1)
 print("Closing in 5 seconds...")
 time.sleep(5)
