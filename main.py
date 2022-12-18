165ebeff8a3db6ff6107776c3a0da5bc4f222602
Hong <107442044+HongGong232@users.noreply.github.com>
2022-12-18 12:29:16 +0000

    GUI
    Made the program into a fully working GUI using Tkinter. I might add it so that people can choose what processes they want to add in as well. Again, the exe is not the updated version.

--- main.py
+++ main.py
@@ -1,91 +1,87 @@
 import os
 import time
 import shutil
+import tkinter as tk
 
-folder = 'C:/Users/'+os.getlogin()+'/AppData/Local/Temp'
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("GD Optimizer")
+        self.geometry("500x500")
+        self.resizable(True, True)
 
-deleteFileCount = 0
-deleteFolderCount = 0
+        # Create label for version
+        self.version = tk.StringVar()
+        self.version.set("(1.1.0)")
+        self.label_version = tk.Label(self, textvariable=self.version)
+        self.label_version.pack()
 
-big_ver = "1"
-mini_ver = ".1.0"
-version = "(" + big_ver + mini_ver + ")"
+        # Create button to run optimization
+        self.button_optimize = tk.Button(self, text="Optimize", command=self.optimize)
+        self.button_optimize.pack()
 
-"""
-Priority Level Value	Priority Level Name
-256	                    Realtime
-128	                    High
-32768	                Above normal
-32	                    Normal
-16384	                Below normal
-64	                    Low
-"""
+        # Create button to run temp cleaner
+        self.button_clean = tk.Button(self, text="Clean Temp", command=self.clean_temp)
+        self.button_clean.pack()
 
-# sets these processes to their respective priorities
-low = ["Discord.exe", "steamwebhelper.exe", "firefox.exe", "chrome.exe"]
-normal = []
-high = []
-real = ["GeometryDash.exe"]
+        # Create label for status messages
+        self.status = tk.StringVar()
+        self.label_status = tk.Label(self, textvariable=self.status)
+        self.label_status.pack()
 
-def lowpriority():
-    for i in low:
-        process_change = 'wmic process where name="' + i + '" CALL setpriority "64"'
-        print(process_change)
-        os.system(process_change)
-        os.system('cls')
+        # Set process priority levels
+        self.low = ["Discord.exe", "steamwebhelper.exe", "firefox.exe", "chrome.exe", "Spotify.exe", "MsMpEng.exe", "brave.exe", "WSHelper.exe"]
+        self.normal = []
+        self.high = []
+        self.real = ["GeometryDash.exe"]
 
-def normalpriority():
-    for i in normal:
-        process_change = 'wmic process where name="' + i + '" CALL setpriority "32"'
-        print(process_change)
-        os.system(process_change)
-        os.system('cls')
+    def optimize(self):
+        self.status.set("Setting process priorities...")
+        self.realpriority()
+        self.lowpriority()
+        self.status.set("All processes set correctly. Make Sure To Re Run This Everytime You Get In A Call")
 
-def highpriority():
-    for i in high:
-        process_change = 'wmic process where name="' + i + '" CALL setpriority "128"'
-        print(process_change)
-        os.system(process_change)
-        os.system('cls')
+    def realpriority(self):
+        for i in self.real:
+            process_change = 'wmic process where name="' + i + '" CALL setpriority "256"'
+            os.system(process_change)
 
-def realpriority(): # sets to high for some reason lmfao
-    for i in real:
-        process_change = 'wmic process where name="' + i + '" CALL setpriority "256"'
-        print(process_change)
-        os.system(process_change)
-        os.system('cls')
+    def lowpriority(self):
+        for i in self.low:
+            process_change = 'wmic process where name="' + i + '" CALL setpriority "64"'
+            os.system(process_change)
 
-def process():
-    realpriority()
-    #highpriority()
-    #normalpriority()
-    lowpriority()
-    return("All processes set correctly.")
+    def clean_temp(self):
+        self.status.set("Running Temp Cleaner...")
 
-print("\033[1;37;40mGD Optimizer Version", version , "\n\033[1;31;40mWarning: You will need to relaunch \nthis everytime you open GD, or join a new discord call.", "\033[1;37;40m\n" + process())
-print("Running Temp Cleaner...")
-time.sleep(3)
+        # Get path to Temp folder
+        folder = f'C:/Users/{os.getlogin()}/AppData/Local/Temp'
 
-for the_file in os.listdir(folder):
-    file_path = os.path.join(folder, the_file)
-    indexNo = file_path.find('\\')
-    itemName = file_path[indexNo+1:]
-    try:
-        if os.path.isfile(file_path):
-            os.unlink(file_path)
-            print( '%s file deleted' % itemName )
-            deleteFileCount = deleteFileCount + 1
+        # Initialize counters for deleted files and folders
+        delete_file_count = 0
+        delete_folder_count = 0
 
-        elif os.path.isdir(file_path):
-            if file_path.__contains__('chocolatey'):  continue
-            shutil.rmtree(file_path)
-            print( '%s folder deleted' % itemName )
-            deleteFolderCount = deleteFolderCount + 1
+        # Iterate through files and directories in Temp folder
+        for the_file in os.listdir(folder):
+            file_path = os.path.join(folder, the_file)
+            index_no = file_path.find('\\')
+            item_name = file_path[index_no+1:]
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                    delete_file_count += 1
 
-    except Exception as e:
-        print('Access Denied: %s' % itemName )
+                elif os.path.isdir(file_path):
+                    if file_path.__contains__('chocolatey'):
+                                           continue
+                    shutil.rmtree(file_path)
+                    delete_folder_count += 1
+            except Exception as e:
+                self.status.set(f'Access Denied: {item_name}')
 
-print(str(deleteFileCount)+' files and '+ str(deleteFolderCount)+' folders deleted.')
-time.sleep(1)
-print("Closing in 5 seconds...")
-time.sleep(5)
+        self.status.set(f'{delete_file_count} files and {delete_folder_count} folders deleted.')
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+
